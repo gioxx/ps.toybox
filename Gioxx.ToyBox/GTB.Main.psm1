18dec87756c1306ace3c094d517bd728da831139
Gioxx <giovanni@solone.it>
2023-08-29 14:19:37 +0200

    Update GTB.Main.psm1
    Sposto qui la variabile caratteri per la priv_MaxLenghtSubString

--- Gioxx.ToyBox/GTB.Main.psm1
+++ Gioxx.ToyBox/GTB.Main.psm1
@@ -1,15 +1,22 @@
-# Vars =============================================================================================================================================================
-
-$GTB = [ordered]@{
-  LicensesJSON = 'https://raw.githubusercontent.com/gioxx/ps.toybox/main/JSON/M365_licenses.json'
-}
-New-Variable -Name GTBVars -Value $GTB -Scope Script -Force
-
+# FUNCTIONS ========================================================================================================================================================
 
 function Update-PS7 {
   iex "& { $(irm https://aka.ms/install-powershell.ps1) } -UseMSI"
 }
 
-# Export Modules ===================================================================================================================================================
+
+# VARS =============================================================================================================================================================
+
+  # Lista licenze M365 utilizzata in Export-MsolAccountSku
+    $GTB = [ordered]@{
+      LicensesJSON = 'https://raw.githubusercontent.com/gioxx/ps.toybox/main/JSON/M365_licenses.json'
+    }
+    New-Variable -Name GTBVars -Value $GTB -Scope Script -Force
+
+  # Quantità caratteri richiamata / usata in priv_MaxLenghtSubString
+    New-Variable -Name MaxFieldLength -Value 35 -Scope Script -Force
+
+
+# EXPORT MODULES ===================================================================================================================================================
 
 Export-ModuleMember -Function "Update-PS7"
