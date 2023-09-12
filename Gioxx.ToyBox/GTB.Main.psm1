ecd71a0f5a7ccbb9277cd1c30340cc765e008b9c
Gioxx <giovanni@solone.it>
2023-09-12 17:01:04 +0200

    Update GTB.Main.psm1
    Reorg.

--- Gioxx.ToyBox/GTB.Main.psm1
+++ Gioxx.ToyBox/GTB.Main.psm1
@@ -1,22 +1,22 @@
+# DATA =============================================================================================================================================================
+$GTB = [ordered]@{
+  LicensesJSON = 'https://raw.githubusercontent.com/gioxx/ps.toybox/main/JSON/M365_licenses.json'
+}
+New-Variable -Name GTBVars -Value $GTB -Scope Script -Force # Lista licenze M365 utilizzata in Export-MsolAccountSku
+
 # FUNCTIONS ========================================================================================================================================================
+# function GTBDebug {
+#   priv_MailSearcher
+# }
 
 function Update-PS7 {
-  iex "& { $(irm https://aka.ms/install-powershell.ps1) } -UseMSI"
+  iex "& { $(irm https://aka.ms/install-powershell.ps1) } -UseMSI" # Aggiornamento PowerShell 7
 }
 
-
 # VARS =============================================================================================================================================================
-
-  # Lista licenze M365 utilizzata in Export-MsolAccountSku
-    $GTB = [ordered]@{
-      LicensesJSON = 'https://raw.githubusercontent.com/gioxx/ps.toybox/main/JSON/M365_licenses.json'
-    }
-    New-Variable -Name GTBVars -Value $GTB -Scope Script -Force
-
-  # Quantità caratteri richiamata / usata in priv_MaxLenghtSubString
-    New-Variable -Name MaxFieldLength -Value 35 -Scope Script -Force
-
+New-Variable -Name MaxFieldLength -Value 35 -Scope Script -Force # Quantità caratteri richiamata / usata in priv_MaxLenghtSubString
 
 # EXPORT MODULES ===================================================================================================================================================
 
+Export-ModuleMember -Function "GTBDebug"
 Export-ModuleMember -Function "Update-PS7"
