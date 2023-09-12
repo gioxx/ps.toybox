8ebcbcf2c0673ef18b5b93de5b9739f903612185
Gioxx <giovanni@solone.it>
2023-09-12 17:00:50 +0200

    Update GTB.Groups.ps1
    Integrazione $eolConnectedCheck, fix refuso (uppercase)

--- Gioxx.ToyBox/Groups/GTB.Groups.ps1
+++ Gioxx.ToyBox/Groups/GTB.Groups.ps1
@@ -17,64 +17,66 @@ function Export-DG {
   Set-Variable ProgressPreference Continue
   $DGsCounter = 0
   $Result = @()
-
-  if ( [string]::IsNullOrEmpty($DG) ) {
-    $All = $True
-  } else {
-    $DGs = Get-DistributionGroup $DG
-  }
-
-  if ($All) {
-    $DGs = Get-DistributionGroup -ResultSize Unlimited
-    $CSV = $True
-  }
+  $eolConnectedCheck = priv_CheckEOLConnection
   
-  if (-not([string]::IsNullOrEmpty($folderCSV))) { $CSV = $True }
-  if ($CSV) { $folder = priv_CheckFolder($folderCSV) }
+  if ( $eolConnectedCheck -eq $true ) {
+    if ( [string]::IsNullOrEmpty($DG) ) { $All = $True } else { $DGs = Get-DistributionGroup $DG }
 
-  $DGs | ForEach {
-    try {
-      $CurrentDG = $_
-      $GetDG = Get-DistributionGroup $CurrentDG
-      $DGsCounter++
-      $PercentComplete = (($DGsCounter / $DGs.Count) * 100)
-      Write-Progress -Activity "Processing $($GetDG.DisplayName)" -Status "$DGsCounter out of $($DGs.Count) ($($PercentComplete.ToString('0.00'))%)" -PercentComplete $PercentComplete
-
-      Get-DistributionGroupMember $CurrentDG | ForEach {
-        if ($All) {
-          $Result += New-Object -TypeName PSObject -Property $([ordered]@{
-            "Group Name" = $GetDG.DisplayName
-            "Group Primary Smtp Address" = $GetDG.PrimarySmtpAddress
-            "Member Display Name" = $_.DisplayName
-            "Member FirstName" = $_.FirstName
-            "Member LastName" = $_.LastName
-            "Member Primary Smtp Address" = $_.PrimarySmtpAddress
-            "Member Company" = $_.Company
-            "Member City" = $_.City
-          })
-        } else {
-          $Result += New-Object -TypeName PSObject -Property $([ordered]@{
-            "Member Display Name" = $_.DisplayName
-            "Member FirstName" = $_.FirstName
-            "Member LastName" = $_.LastName
-            "Member Primary Smtp Address" = $_.PrimarySmtpAddress
-            "Member Company" = $_.Company
-            "Member City" = $_.City
-          })
-        }
-      }
-    } catch {
-      Write-Error $_.Exception.Message
+    if ($All) {
+      $DGs = Get-DistributionGroup -ResultSize Unlimited
+      $CSV = $True
     }
-  }
+    
+    if (-not([string]::IsNullOrEmpty($folderCSV))) { $CSV = $True }
+    if ($CSV) { $folder = priv_CheckFolder($folderCSV) }
 
-  if ( $GridView ) {
-    $Result | Out-GridView -Title "M365 Distribution Groups"
-  } elseif ( $CSV ) {
-    $CSVfile = priv_SaveFileWithProgressiveNumber("$($folder)\$((Get-Date -format "yyyyMMdd").ToString())_M365-DistributionGroups-Report.csv")
-    $Result | Export-CSV $CSVfile -NoTypeInformation -Encoding UTF8 -Delimiter ";"
+    $DGs | ForEach {
+      try {
+        $CurrentDG = $_
+        $GetDG = Get-DistributionGroup $CurrentDG
+        $DGsCounter++
+        $PercentComplete = (($DGsCounter / $DGs.Count) * 100)
+        Write-Progress -Activity "Processing $($GetDG.DisplayName)" -Status "$DGsCounter out of $($DGs.Count) ($($PercentComplete.ToString('0.00'))%)" -PercentComplete $PercentComplete
+
+        Get-DistributionGroupMember $CurrentDG | ForEach {
+          if ($All) {
+            $Result += New-Object -TypeName PSObject -Property $([ordered]@{
+              "Group Name" = $GetDG.DisplayName
+              "Group Primary Smtp Address" = $GetDG.PrimarySmtpAddress
+              "Member Display Name" = $_.DisplayName
+              "Member FirstName" = $_.FirstName
+              "Member LastName" = $_.LastName
+              "Member Primary Smtp Address" = $_.PrimarySmtpAddress
+              "Member Company" = $_.Company
+              "Member City" = $_.City
+            })
+          } else {
+            $Result += New-Object -TypeName PSObject -Property $([ordered]@{
+              "Member Display Name" = $_.DisplayName
+              "Member FirstName" = $_.FirstName
+              "Member LastName" = $_.LastName
+              "Member Primary Smtp Address" = $_.PrimarySmtpAddress
+              "Member Company" = $_.Company
+              "Member City" = $_.City
+            })
+          }
+        }
+      } catch {
+        Write-Error $_.Exception.Message
+      }
+    }
+
+    if ( $GridView ) {
+      $Result | Out-GridView -Title "M365 Distribution Groups"
+    } elseif ( $CSV ) {
+      $CSVfile = priv_SaveFileWithProgressiveNumber("$($folder)\$((Get-Date -format "yyyyMMdd").ToString())_M365-DistributionGroups-Report.csv")
+      $Result | Export-CSV $CSVfile -NoTypeInformation -Encoding UTF8 -Delimiter ";"
+    } else {
+      $Result
+    }
+
   } else {
-    $Result
+    Write-Host "`nCan't connect or use Microsoft Exchange Online Management module. `nPlease check logs." -f "Red"
   }
 }
 
@@ -95,64 +97,66 @@ function Export-DDG {
   Set-Variable ProgressPreference Continue
   $DDGsCounter = 0
   $Result = @()
+  $eolConnectedCheck = priv_CheckEOLConnection
 
-  if ( [string]::IsNullOrEmpty($DDG) ) {
-    $All = $True
-  } else {
-    $DDGs = Get-DynamicDistributionGroup $DDG
-  }
+  if ( $eolConnectedCheck -eq $true ) {
+    if ( [string]::IsNullOrEmpty($DDG) ) { $All = $True } else { $DDGs = Get-DynamicDistributionGroup $DDG }
 
-  if ($All) {
-    $DDGs = Get-DynamicDistributionGroup -ResultSize Unlimited
-    $CSV = $True
-  }
-  
-  if (-not([string]::IsNullOrEmpty($folderCSV))) { $CSV = $True }
-  if ($CSV) { $folder = priv_CheckFolder($folderCSV) }
-
-  $DDGs | ForEach {
-    try {
-      $CurrentDDG = $_
-      $GetDDG = Get-DynamicDistributionGroup $CurrentDDG
-      $DDGsCounter++
-      $PercentComplete = (($DDGsCounter / $DDGs.Count) * 100)
-      Write-Progress -Activity "Processing $($GetDDG.DisplayName)" -Status "$DDGsCounter out of $($DDGs.Count) ($($PercentComplete.ToString('0.00'))%)" -PercentComplete $PercentComplete
-
-      Get-DynamicDistributionGroupMember $CurrentDDG | ForEach {
-        if ($All) {
-          $Result += New-Object -TypeName PSObject -Property $([ordered]@{
-            "Group Name" = $GetDDG.DisplayName
-            "Group Primary Smtp Address" = $GetDDG.PrimarySmtpAddress
-            "Member Display Name" = $_.DisplayName
-            "Member FirstName" = $_.FirstName
-            "Member LastName" = $_.LastName
-            "Member Primary Smtp Address" = $_.PrimarySmtpAddress
-            "Member Company" = $_.Company
-            "Member City" = $_.City
-          })
-        } else {
-          $Result += New-Object -TypeName PSObject -Property $([ordered]@{
-            "Member Display Name" = $_.DisplayName
-            "Member FirstName" = $_.FirstName
-            "Member LastName" = $_.LastName
-            "Member Primary Smtp Address" = $_.PrimarySmtpAddress
-            "Member Company" = $_.Company
-            "Member City" = $_.City
-          })
-        }
-      }
-    } catch {
-      Write-Error $_.Exception.Message
+    if ($All) {
+      $DDGs = Get-DynamicDistributionGroup -ResultSize Unlimited
+      $CSV = $True
     }
-  }
+    
+    if (-not([string]::IsNullOrEmpty($folderCSV))) { $CSV = $True }
+    if ($CSV) { $folder = priv_CheckFolder($folderCSV) }
 
-  if ( $GridView ) {
-    $Result | Out-GridView -Title "M365 Dynamic Distribution Groups"
-  } elseif ( $CSV ) {
-    $CSVfile = priv_SaveFileWithProgressiveNumber("$($folder)\$((Get-Date -format "yyyyMMdd").ToString())_M365-DynamicDistributionGroups-Report.csv")
-    $Result | Export-CSV $CSVfile -NoTypeInformation -Encoding UTF8 -Delimiter ";"
+    $DDGs | ForEach {
+      try {
+        $CurrentDDG = $_
+        $GetDDG = Get-DynamicDistributionGroup $CurrentDDG
+        $DDGsCounter++
+        $PercentComplete = (($DDGsCounter / $DDGs.Count) * 100)
+        Write-Progress -Activity "Processing $($GetDDG.DisplayName)" -Status "$DDGsCounter out of $($DDGs.Count) ($($PercentComplete.ToString('0.00'))%)" -PercentComplete $PercentComplete
+
+        Get-DynamicDistributionGroupMember $CurrentDDG | ForEach {
+          if ($All) {
+            $Result += New-Object -TypeName PSObject -Property $([ordered]@{
+              "Group Name" = $GetDDG.DisplayName
+              "Group Primary Smtp Address" = $GetDDG.PrimarySmtpAddress
+              "Member Display Name" = $_.DisplayName
+              "Member FirstName" = $_.FirstName
+              "Member LastName" = $_.LastName
+              "Member Primary Smtp Address" = $_.PrimarySmtpAddress
+              "Member Company" = $_.Company
+              "Member City" = $_.City
+            })
+          } else {
+            $Result += New-Object -TypeName PSObject -Property $([ordered]@{
+              "Member Display Name" = $_.DisplayName
+              "Member FirstName" = $_.FirstName
+              "Member LastName" = $_.LastName
+              "Member Primary Smtp Address" = $_.PrimarySmtpAddress
+              "Member Company" = $_.Company
+              "Member City" = $_.City
+            })
+          }
+        }
+      } catch {
+        Write-Error $_.Exception.Message
+      }
+    }
+
+    if ( $GridView ) {
+      $Result | Out-GridView -Title "M365 Dynamic Distribution Groups"
+    } elseif ( $CSV ) {
+      $CSVfile = priv_SaveFileWithProgressiveNumber("$($folder)\$((Get-Date -format "yyyyMMdd").ToString())_M365-DynamicDistributionGroups-Report.csv")
+      $Result | Export-CSV $CSVfile -NoTypeInformation -Encoding UTF8 -Delimiter ";"
+    } else {
+      $Result
+    }
+
   } else {
-    $Result
+    Write-Host "`nCan't connect or use Microsoft Exchange Online Management module. `nPlease check logs." -f "Red"
   }
 }
 
@@ -173,64 +177,66 @@ function Export-M365Group {
   Set-Variable ProgressPreference Continue
   $M365GsCounter = 0
   $Result = @()
+  $eolConnectedCheck = priv_CheckEOLConnection
 
-  if ( [string]::IsNullOrEmpty($M365G) ) {
-    $All = $True
-  } else {
-    $M365Gs = Get-UnifiedGroup $M365G
-  }
+  if ( $eolConnectedCheck -eq $true ) {
+    if ( [string]::IsNullOrEmpty($M365G) ) { $All = $True } else { $M365Gs = Get-UnifiedGroup $M365G }
 
-  if ($All) {
-    $M365Gs = Get-UnifiedGroup -ResultSize Unlimited
-    $CSV = $True
-  }
-  
-  if (-not([string]::IsNullOrEmpty($folderCSV))) { $CSV = $True }
-  if ($CSV) { $folder = priv_CheckFolder($folderCSV) }
-
-  $M365Gs | ForEach {
-    try {
-      $CurrentM365G = $_
-      $GetM365G = Get-UnifiedGroup $CurrentM365G
-      $M365GsCounter++
-      $PercentComplete = (($M365GsCounter / $M365Gs.Count) * 100)
-      Write-Progress -Activity "Processing $($GetM365G.DisplayName)" -Status "$M365GsCounter out of $($M365Gs.Count) ($($PercentComplete.ToString('0.00'))%)" -PercentComplete $PercentComplete
-
-      $GetM365G | Get-UnifiedGroupLinks -LinkType Member | ForEach {
-        if ($All) {
-          $Result += New-Object -TypeName PSObject -Property $([ordered]@{
-            "Group Name" = $GetM365G.DisplayName
-            "Group Primary Smtp Address" = $GetM365G.PrimarySmtpAddress
-            "Member Display Name" = $_.DisplayName
-            "Member FirstName" = $_.FirstName
-            "Member LastName" = $_.LastName
-            "Member Primary Smtp Address" = $_.PrimarySmtpAddress
-            "Member Company" = $_.Company
-            "Member City" = $_.City
-          })
-        } else {
-          $Result += New-Object -TypeName PSObject -Property $([ordered]@{
-            "Member Display Name" = $_.DisplayName
-            "Member FirstName" = $_.FirstName
-            "Member LastName" = $_.LastName
-            "Member Primary Smtp Address" = $_.PrimarySmtpAddress
-            "Member Company" = $_.Company
-            "Member City" = $_.City
-          })
-        }
-      }
-    } catch {
-      Write-Error $_.Exception.Message
+    if ($All) {
+      $M365Gs = Get-UnifiedGroup -ResultSize Unlimited
+      $CSV = $True
     }
-  }
+    
+    if (-not([string]::IsNullOrEmpty($folderCSV))) { $CSV = $True }
+    if ($CSV) { $folder = priv_CheckFolder($folderCSV) }
 
-  if ( $GridView ) {
-    $Result | Out-GridView -Title "M365 Unified Groups"
-  } elseif ( $CSV ) {
-    $CSVfile = priv_SaveFileWithProgressiveNumber("$($folder)\$((Get-Date -format "yyyyMMdd").ToString())_M365-UnifiedGroups-Report.csv")
-    $Result | Export-CSV $CSVfile -NoTypeInformation -Encoding UTF8 -Delimiter ";"
+    $M365Gs | ForEach {
+      try {
+        $CurrentM365G = $_
+        $GetM365G = Get-UnifiedGroup $CurrentM365G
+        $M365GsCounter++
+        $PercentComplete = (($M365GsCounter / $M365Gs.Count) * 100)
+        Write-Progress -Activity "Processing $($GetM365G.DisplayName)" -Status "$M365GsCounter out of $($M365Gs.Count) ($($PercentComplete.ToString('0.00'))%)" -PercentComplete $PercentComplete
+
+        $GetM365G | Get-UnifiedGroupLinks -LinkType Member | ForEach {
+          if ($All) {
+            $Result += New-Object -TypeName PSObject -Property $([ordered]@{
+              "Group Name" = $GetM365G.DisplayName
+              "Group Primary Smtp Address" = $GetM365G.PrimarySmtpAddress
+              "Member Display Name" = $_.DisplayName
+              "Member FirstName" = $_.FirstName
+              "Member LastName" = $_.LastName
+              "Member Primary Smtp Address" = $_.PrimarySmtpAddress
+              "Member Company" = $_.Company
+              "Member City" = $_.City
+            })
+          } else {
+            $Result += New-Object -TypeName PSObject -Property $([ordered]@{
+              "Member Display Name" = $_.DisplayName
+              "Member FirstName" = $_.FirstName
+              "Member LastName" = $_.LastName
+              "Member Primary Smtp Address" = $_.PrimarySmtpAddress
+              "Member Company" = $_.Company
+              "Member City" = $_.City
+            })
+          }
+        }
+      } catch {
+        Write-Error $_.Exception.Message
+      }
+    }
+
+    if ( $GridView ) {
+      $Result | Out-GridView -Title "M365 Unified Groups"
+    } elseif ( $CSV ) {
+      $CSVfile = priv_SaveFileWithProgressiveNumber("$($folder)\$((Get-Date -format "yyyyMMdd").ToString())_M365-UnifiedGroups-Report.csv")
+      $Result | Export-CSV $CSVfile -NoTypeInformation -Encoding UTF8 -Delimiter ";"
+    } else {
+      $Result
+    }
+
   } else {
-    $Result
+    Write-Host "`nCan't connect or use Microsoft Exchange Online Management module. `nPlease check logs." -f "Red"
   }
 }
 
@@ -244,6 +250,7 @@ function Get-UserGroups {
   )
 
   $mggConnectedCheck = priv_CheckMGGraphModule
+
   if ( $mggConnectedCheck -eq $true ) {
     $groupList=@()
 
@@ -284,7 +291,7 @@ function Get-UserGroups {
     if ( $GridView ) { $groupList | Out-GridView -Title "M365 User Groups" } else { $groupList }
 
   } else {
-    Write-Host "`nCan't connect or use Microsoft Graph Modules. `nPlease check logs." -f "Red"
+    Write-Host "`nCan't connect or use Microsoft Graph modules. `nPlease check logs." -f "Red"
   }
 }
 
