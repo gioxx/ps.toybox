79a81358ada3eadf1c1b58fcde4e5a9ba06b71a1
Gioxx <giovanni@solone.it>
2023-12-20 15:43:38 +0100

    User-CloseAllPSSessions

--- Gioxx.ToyBox/Main/GTB.Xtras.ps1
+++ Gioxx.ToyBox/Main/GTB.Xtras.ps1
@@ -44,3 +44,22 @@ Set-PSReadlineKeyHandler -Key F7 -BriefDescription HistoryList -Description "Sho
         [Microsoft.PowerShell.PSConsoleReadLine]::Insert(($command -join "`n"))
     }
 }
+
+# Credits
+# https://www.reddit.com/r/PowerShell/comments/160185w/assembly_with_same_name_is_already_loaded/
+# https://reddit.com/r/PowerShell/comments/160185w/comment/k69dgyn
+# [System.AppDomain]::CurrentDomain.GetAssemblies() | Where-Object Location | Sort-Object -Property FullName | Select-Object -Property FullName, Location, GlobalAssemblyCache, IsFullyTrusted
+function User-CloseAllPSSessions {
+    param()
+    $PSSessions = Get-PSSession
+    if ($PSSessions) {
+        ForEach ($Session in $PSSessions) { 
+            Remove-PSSession $Session.Id 
+        }
+    }
+    Disconnect-ExchangeOnline -Confirm:$false
+}
+
+# EXPORT MODULES ===================================================================================================================================================
+
+Export-ModuleMember -Function "User-CloseAllPSSessions"
